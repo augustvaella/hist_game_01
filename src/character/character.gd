d6d2728ac4fc344d6f7c3e84ac604c2ae24052ed
augustvaella <nikkososaku@akane88.sakura.ne.jp>
2025-01-06 06:10:06 +0900

    Character.listened_object

--- src/character/character.gd
+++ src/character/character.gd
@@ -10,32 +10,8 @@ signal listened_object(object: Object)
 @export var chat_texture: Texture2D
 @export var duel_texture: Texture2D
 
-var _changed_callables: Array[Callable]
-
-
 func _setup():
 	changed.connect(func(): listened_object.emit(Changed.new()))
-
-
-func call_changed():
-	_changed_callables.map(func(c): c.call(Changed.new()))
-
-
-func set_listen_object(callable: Callable):
-	#_changed_callables.append(callable)
-	listened_object.connect(callable)
-
-
-func cancel_listen_object(callable: Callable):
-	#_changed_callables.erase(callable)
-	listened_object.disconnect(callable)
-
-
-func reset_listen_object():
-	_changed_callables.clear()
-
-	for c in listened_object.get_connections():
-		listened_object.disconnect(c)
 
 
 # to be overriden
